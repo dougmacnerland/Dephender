0cf06ac054ffcfcabed4523314e82d5a45da78a0
Doug MacNerland <macnerl2@illinois.edu>
2015-11-09 10:20:27 -0600

    commented

--- app/src/main/java/me/macnerland/dephender/Main.java
+++ app/src/main/java/me/macnerland/dephender/Main.java
@@ -26,19 +26,34 @@ public class Main extends Activity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         SharedPreferences pref = this.getPreferences(MODE_PRIVATE);
-
+        
+        /*Create an activity*/
         super.onCreate(savedInstanceState);
+	
+	/*Initialize OpenGLES renderer
+ * 	with variables Activity and SharedPreferences*/
         menu = new MenuRenderer(this, pref);
 
-
+        /*Request fullscreen, cull menu bar*/
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);//runs fullscreen
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+	/*Set the android draw content to main activity layout
+ * 	Specified at res/layout/activity_main*/
         setContentView(R.layout.activity_main);
 
+	/*Initialize new AdView, mAdview, from the gadView ID in the
+ * 	activity_main layout*/
         AdView mAdView= (AdView) findViewById(R.id.gadView);
-        AdRequest adRequest = new AdRequest.Builder().build();//do not use  for testing
+        
+        /*Request an ad*/
+	/*Actual deployment*/
+        AdRequest adRequest = new AdRequest.Builder().build();
+	/*Testing on Emulator*/
         //AdRequest adRequest = new AdRequest.Builder()
         //        .addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+        
+	/*Load the request into the ad*/
         mAdView.loadAd(adRequest);
 
         //glSurfaceView = new MenuSurface(this);
